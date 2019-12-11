1666756734a95d2df14664923aded9b117a5575b
Claudia Gutarra <claudiaagutarra@gmail.com>
2019-12-10 23:29:00 -0500

    db files created

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("I've Created a Muenster Burger", "true"),
+("If Looks Could Kale Burger", "true"),
+("Little Swiss Bunshine Burger", "true"),
+("Onion-tended Consequences", "false"),
+("Bruschetta Bout It Burger", "false"),
+("To Thine Own Self be Bleu Burger", "false"),
+("Say It Ain't Cilantro Burger", "false"),
+("If You've Got It, Croissant It Burger", "true"),
+("It Takes Two to Mango", "false"),
+
+
+
+
