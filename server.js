88976ce3ead76487592711a66466a962ba9263a0
Claudia Gutarra <claudiaagutarra@gmail.com>
2020-02-28 05:15:03 -0500

    heroku edits

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 
+
 var PORT = process.env.PORT || 8000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
+
+
 app.use(express.static("public"));
 
 // Parse application body
